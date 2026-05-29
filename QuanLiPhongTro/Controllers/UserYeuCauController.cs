909f5dc2dc769a6ab22a558b42a622a1291d314b
Team <team@project.com>
2026-05-29 18:49:16 +0000

    Lịch sử và Hóa đơn khách hàng (User)
    - UserHopDongController.cs + views: xem hợp đồng cá nhân
    - UserThanhToanController.cs + views: danh sách hóa đơn cần đóng tiền
    - UserYeuCauController.cs + views: gửi và theo dõi yêu cầu sửa chữa

---
+++ QuanLiPhongTro/Controllers/UserYeuCauController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLiPhongTro.Controllers
+{
+    public class UserYeuCauController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
